97059985e6b1d21e2102b275d3668b85ed6e4ad3
Grigoriy Skobelev <gskobelevff@gmail.com>
2020-09-11 10:45:21 +0300

    RndConf

--- demo-kafka-streams/src/main/java/com/skobelev/gskoba/GskobaApplicationKafkaStreams.java
+++ demo-kafka-streams/src/main/java/com/skobelev/gskoba/GskobaApplicationKafkaStreams.java
@@ -8,12 +8,19 @@ import org.apache.kafka.streams.Topology;
 import org.apache.kafka.streams.kstream.Consumed;
 import org.apache.kafka.streams.kstream.KStream;
 import org.apache.kafka.streams.kstream.Produced;
+import org.apache.kafka.streams.processor.Processor;
+import org.apache.kafka.streams.processor.ProcessorContext;
+import org.apache.kafka.streams.processor.ProcessorSupplier;
+import org.apache.kafka.streams.state.KeyValueStore;
+import org.apache.kafka.streams.state.StoreBuilder;
+import org.apache.kafka.streams.state.Stores;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.List;
 import java.util.Properties;
 
 @SpringBootApplication
@@ -40,11 +47,20 @@ class Config {
     }
 
     @Bean
-    public Topology topology(StreamsBuilder streamsBuilder) {
-        KStream<String, String> inputTopic = streamsBuilder.stream("first_topic",
+    public Topology topology(StreamsBuilder streamsBuilder,
+                             ProcessorSupplier<String, String> batchProcessor,
+                             StoreBuilder storeBuilders) {
+
+            streamsBuilder.addStateStore(storeBuilders);
+
+        KStream<String, String> firstTopic = streamsBuilder.stream("first_topic",
                 Consumed.with(Serdes.String(), Serdes.String()));
-        inputTopic.mapValues(value -> value + " DevParty 2020 !!!!").to("second_topic",
+        firstTopic.mapValues(value -> value + " RndConf 2020 !!!!").to("second_topic",
                 Produced.with(Serdes.String(), Serdes.String()));
+
+        streamsBuilder.stream("second_topic",
+                Consumed.with(Serdes.String(), Serdes.String())).process(batchProcessor, storeBuilders.name());
+
         return streamsBuilder.build();
     }
 
@@ -52,5 +68,45 @@ class Config {
     public StreamsBuilder streamsBuilder() {
         return new StreamsBuilder();
     }
+
+    @Bean
+    public ProcessorSupplier<String, String> batchProcessor() {
+        return () -> new BatchProcessor();
+    }
+
+    @Bean
+    public StoreBuilder store() {
+        return Stores.keyValueStoreBuilder(Stores.persistentKeyValueStore("test-store"),
+                Serdes.String(), Serdes.String());
+    }
 }
 
+
+class BatchProcessor implements Processor<String, String> {
+
+    private ProcessorContext context;
+    private KeyValueStore<String, String> store;
+
+    @Override
+    public void init(ProcessorContext context) {
+        this.context = context;
+        this.store = (KeyValueStore<String, String>) context.getStateStore("test-store");
+    }
+
+    @Override
+    public void process(String key, String value) {
+        store.put(key, value);
+        if (store.approximateNumEntries() >= 3) {
+            store.all().forEachRemaining(keyValue -> {
+                System.out.println(keyValue.value);
+            });
+            context.commit();
+            store.flush();
+        }
+    }
+
+    @Override
+    public void close() {
+        //do nothing
+    }
+}
